55ea2f49bebf5c72589d12cc3c20cd5b91cb4a1b
Christian Pojoni <34570565+5queezer@users.noreply.github.com>
2026-05-01 16:04:16 +0200

    Remove StrictMode wrapper from production hydration entry
    Result: {"status":"keep","bundle_gzip_kb":140.591,"bundle_raw_kb":467.185,"js_files":9,"css_kb":0}

--- src/client.tsx
+++ src/client.tsx
@@ -1,12 +1,7 @@
 import { StartClient } from '@tanstack/react-start/client'
-import { StrictMode, startTransition } from 'react'
+import { startTransition } from 'react'
 import { hydrateRoot } from 'react-dom/client'
 
 startTransition(() => {
-  hydrateRoot(
-    document,
-    <StrictMode>
-      <StartClient />
-    </StrictMode>,
-  )
+  hydrateRoot(document, <StartClient />)
 })
