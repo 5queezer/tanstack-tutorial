36805d5330846d348baa26fecd6aeabdd04797b5
Christian Pojoni <34570565+5queezer@users.noreply.github.com>
2026-05-01 16:04:31 +0200

    Hydrate StartClient directly without startTransition wrapper
    Result: {"status":"keep","bundle_gzip_kb":140.571,"bundle_raw_kb":467.15,"js_files":9,"css_kb":0}

--- src/client.tsx
+++ src/client.tsx
@@ -1,7 +1,4 @@
 import { StartClient } from '@tanstack/react-start/client'
-import { startTransition } from 'react'
 import { hydrateRoot } from 'react-dom/client'
 
-startTransition(() => {
-  hydrateRoot(document, <StartClient />)
-})
+hydrateRoot(document, <StartClient />)
